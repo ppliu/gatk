e9af7bfcb11f99d9e527a882ff0cdd6599e0f595
Gabriel Pratt <gpratt@ucsd.edu>
2013-05-13 16:50:57 -0700

    updated scripts to new locations

--- public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/GenomeCoverageBed.scala
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/GenomeCoverageBed.scala
@@ -4,8 +4,32 @@ import org.broadinstitute.sting.commandline._
 import java.io.File
 import org.broadinstitute.sting.queue.function.CommandLineFunction
 
-class GenomeCoverageBed(@Input inBam: File, @Argument genomeSize: String, @Output bedGraph: File, @Argument strand: String = null) extends CommandLineFunction {
-  //When it comes time to refactor use the @Input(doc, requiered=False) pattern...
-  def commandLine = "genomeCoverageBed " + optional("-strand", strand) + required("-split") + required("-bg") + required("-ibam", inBam) + required("-g", genomeSize) + " > " + bedGraph
-  this.isIntermediate = true
+class GenomeCoverageBed extends CommandLineFunction {
+
+ @Input(doc="input bed file", shortName = "inBed", fullName = "input_bed_files", required = false) 
+ var inBed: File = _
+ 
+ @Input(doc="input bam file", shortName = "inBam", fullName = "input_bam_files", required = false) 
+ var inBam: File = _
+ 
+ @Argument(doc="file contating chromosome sizes", shortName = "genomeSize", fullName = "chromosome_sizes", required = true) 
+ var genomeSize: String = _
+ 
+ @Output(doc="output bed graph file", shortName = "bedGraph", fullName = "out_bed_graph", required = true) 
+ var bedGraph: File = _
+ 
+ @Argument(doc="use a single strand when generating the bed graph (+ / -)", shortName = "strand", fullName = "strand", required = false) 
+ var strand: String = null
+ 
+ @Argument(doc="split gapped alignments", shortName = "split", fullName = "split", required = false) 
+ var split: Boolean = true 
+
+ def commandLine = "genomeCoverageBed " + 
+  		    optional("-strand", strand) + 
+		    conditional(split, "-split") + 
+		    required("-bg") + 
+		    optional("-ibam", inBam) +
+		    optional("-i", inBed) +  
+		    required("-g", genomeSize) + " > " + bedGraph
+ this.isIntermediate = true
 }
