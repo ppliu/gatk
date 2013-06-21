72dbea31fe70e561825a24e928a486083eae8ed9
Gabriel Pratt <gpratt@ucsd.edu>
2013-06-20 19:34:38 -0700

    updated repetative regions to be the python version

--- public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/FilterRepetativeRegions.scala
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/FilterRepetativeRegions.scala
@@ -7,6 +7,6 @@ import org.broadinstitute.sting.queue.function.CommandLineFunction
 
 class FilterRepetativeRegions(@Input inFastq: File, @Output outCounts: File, @Output outNoRep: File) extends CommandLineFunction {
 
-  def commandLine = "bowtie -S -q -p 4 -e 100 -l 20 --un %s all_ref %s | grep -v \"@\" | perl /nas3/yeolab/Software/pipeflower/count_aligned_from_sam.pl > %s".format(outNoRep, inFastq, outCounts)
+  def commandLine = "bowtie -S -q -p 4 -e 100 -l 20 --un %s all_ref %s | count_aligned_from_sam.py > %s".format(outNoRep, inFastq, outCounts)
 
 }
