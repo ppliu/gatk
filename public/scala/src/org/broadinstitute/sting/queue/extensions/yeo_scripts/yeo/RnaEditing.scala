68e4c22f6b90be4096d21018286f34a8caeb59b4
Gabriel Pratt <gpratt@ucsd.edu>
2014-02-10 10:16:29 -0800

    added editing and miso to pipeline

---
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/RnaEditing.scala
@@ -0,0 +1,16 @@
+package org.broadinstitute.sting.queue.extensions.yeo
+
+import org.broadinstitute.sting.commandline._
+import java.io.File
+import org.broadinstitute.sting.queue.function.CommandLineFunction
+
+class RnaEditing(@Input inBam: File, @Argument snpEffDb: String, @Argument snpDb: String, @Argument genome: String, @Argument flipped: String, @Output output: File) extends CommandLineFunction {
+
+  def commandLine = "create_rna_editing_makefile.py " +
+      		     required("--bam", inBam) +
+		     required("--snpEffDb", snpEffDb) +
+		     required("--snpDb", snpDb) +
+		     required("--genome", genome) +
+		     required("--flipped", flipped) + 
+		     required("--outfile", output) + " && make -j 2 -f " + output    			           
+	}		     
