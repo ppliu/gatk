c570097d5095b1e4fdfa03a7d83380093832afce
Gabriel Pratt <gpratt@ucsd.edu>
2014-02-17 14:11:31 -0800

    added new clip-seq peak calling methods

---
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/Piranha.scala
@@ -0,0 +1,22 @@
+package org.broadinstitute.sting.queue.extensions.yeo
+
+import org.broadinstitute.sting.commandline._
+import java.io.File
+import org.broadinstitute.sting.queue.function.CommandLineFunction
+
+class Piranha extends CommandLineFunction {
+
+ @Input(doc="input bam file", shortName = "inBam", fullName = "input_bam_file", required = true)
+ var inBam: File = _
+
+ @Output(doc="bed file of peaks to output", shortName = "outBed", fullName = "outBed", required = true)
+ var outBed: File = _
+ 
+  override def shortDescription = "Piranah"
+  
+  def commandLine = "run_piranah.py " +
+    required("-b", inBam) +
+    required("-s", 100) +
+    required("-p", .05) +
+    required("-o", outBed)
+}
