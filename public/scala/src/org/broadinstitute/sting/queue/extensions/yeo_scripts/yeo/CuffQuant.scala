0a3629feacccf70fef6c0260eb6d3e0dc511b3ed
ppliu <uil.kcirtap@gmail.com>
2014-06-09 13:57:24 -0700

    cuffquant tool def

---
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/CuffQuant.scala
@@ -0,0 +1,31 @@
+package org.broadinstitute.sting.queue.extensions.yeo
+
+import org.broadinstitute.sting.commandline._
+import java.io.File
+import org.broadinstitute.sting.queue.function.CommandLineFunction
+
+class CuffQuant extends CommandLineFunction {
+
+ 
+ @Input(doc="input bam file", shortName = "inBam", fullName = "input_bam_file", required = true) 
+ var inBam: File = _
+
+ @Output(doc="output sam file", shortName = "outSam", fullName = "out_sam_file", required = true) 
+ var outSam: File = _
+ 
+ @Argument(doc="genome location", shortName = "genome", fullName = "genome", required = true) 
+ var genome: String = _
+
+ @Argument(doc="library type", shortName = "ltype", fullName = "librarytype", required = true) 
+ var ltype: String = _
+
+ this.nCoresRequest = Option(16) 
+ override def shortDescription = "CuffQuant"  
+ def commandLine = "cuffquant " +
+  		required("--library-type", ltype) +
+  		required("--num-threads", "16") +
+  		required(genome) +
+  		required(inBam) 
+		
+ //this.isIntermediate = true
+}
