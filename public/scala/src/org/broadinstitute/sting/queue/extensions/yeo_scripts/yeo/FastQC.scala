1b47057feb8c2d11fe2bda4782b622d18957c5ba
ppliu <uil.kcirtap@gmail.com>
2014-07-08 11:21:13 -0700

    changed fastqc to use ace/prog perl

--- public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/FastQC.scala
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/yeo_scripts/yeo/FastQC.scala
@@ -4,8 +4,10 @@ import java.io.File
 import org.broadinstitute.sting.queue.function.CommandLineFunction
 import org.broadinstitute.sting.commandline.Input
 
+
 class FastQC(@Input inFastq: File) extends CommandLineFunction {
+  def perl = "/oasis/tscc/scratch/yeo-lab/ace_progenity/asd_processing/dependencies/perl-5.20.0/perl"
   override def shortDescription = "FastQC"
-  def commandLine = "fastqc %s".format(inFastq)
+  def commandLine = "%s fastqc %s".format(perl, inFastq)
 
 }
